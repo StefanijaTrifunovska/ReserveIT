a10a1c2e3d846488b3559de1d24108c8b750ed2a
Dea Pahumi <pahumi84@gmail.com>
2020-12-20 20:17:44 +0100

    Homework3

--- Homework3/reserveitapp-vtoraverzija/src/User/Dashboard.js
+++ Homework3/reserveitapp-vtoraverzija/src/User/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Dashboard.scss';
+import {Link} from 'react-router-dom';
 
 function Dashboard() {
     return (
@@ -7,22 +8,27 @@ function Dashboard() {
             <div className="dashboard-container">
                 <div className="dashboard-top">
                   <h4>My dashboard</h4>
-                  <h4>Make a reservation</h4>
+                      <Link to="/reserve">
+                  <h4><div className="white">Make a reservation</div></h4>
+            </Link>    
                 </div>
                 <div className="nav-reservations">
                     <div className="current">
                     My reserervations
                     </div>
-                    <div className="edit">
-                        Edit 
-                    </div>
                 </div>
                 <div className="info-reservations">
                     <div className="place">
-                        Place:
+                        Place: 
+                    </div>Kafe Bar Sindikat
+                    <div className="date">Date: 
+                    </div>31.12.2020
+                    <div className="time">Time: </div>14:30
+                    <div className="edit">
+                    <Link to="/reserve">
+              <div className="white">Edit</div>
+            </Link>
                     </div>
-                    <div className="date">Date:</div>
-                    <div className="time">Time:</div>
                 </div>
             </div>
         </div>

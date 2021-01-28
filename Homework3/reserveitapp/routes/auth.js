c5a9c6acbb09b62d1479dd32a538642dadbd9459
Ana Tasevska <tasevska.anna@yahoo.com>
2021-01-28 22:56:12 +0100

    final version

--- Homework3/reserveitapp/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const Controller = require('C:/Users/Stefanija/Desktop/ReserveIT/Homework3/reserveitapp/src/models/Controller')
-
-router.post('/register', Controller.register)
-router.post('/login', Controller.login)
-module.exports = router
